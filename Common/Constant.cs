d465823ae5a1b976d85ecca92dabb706c5cc4d1d
Garfield Jiang <jehovah0121@gmail.com>
2020-10-05 19:03:57 +0800

    Update master version to 1.1.0

--- Common/Constant.cs
+++ Common/Constant.cs
@@ -4,7 +4,7 @@ namespace COL.UnityGameWheels.Unity
 {
     public static class Constant
     {
-        public static readonly Version Version = new Version("1.0.0");
+        public static readonly Version Version = new Version("1.1.0");
 
         public static readonly Version CoreVersion = Core.Constant.Version;
     }
